6ea2200952ccef9b4e594f442f110a58c182de61
kiran k <kiranbasha445@gmai.com>
2023-07-30 19:00:07 +0530

    addedbootstrapandmenu

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-        hello
-    </div>
-  )
+    <>
+      <div className="container">
+        <div className="row row-cols-2">
+          <div className="col-12 bg-primary">nav</div>
+          <div className="col-lg-3 d-none d-lg-block bg-secondary">aside</div>
+          <div className="col-lg-9 col-12 bg-danger">main</div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
